a4201e73c8089eead5ba592e14ed88ad5bfa12d7
Mingxun Wang <mwang87@gmail.com>
2020-08-10 10:11:39 -0700

    updating run server for gunicorn

--- run_server.sh
+++ run_server.sh
@@ -3,5 +3,5 @@
 
 #curlftpfs ftp://massive.ucsd.edu/ /data/massive/
 
-python ./app.py
-#gunicorn -w 1 --threads=6 --worker-class=gthread -b 0.0.0.0:5000 --timeout 3600 app:server --access-logfile /app/logs/access.log
+#python ./app.py
+gunicorn -w 4 --threads=6 --worker-class=gthread -b 0.0.0.0:5000 --timeout 3600 app:server --access-logfile /app/logs/access.log
