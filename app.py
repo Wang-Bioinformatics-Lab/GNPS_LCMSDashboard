948386f1b66ceef5098a872cc3435b1685520ec7
Mingxun Wang <mwang87@gmail.com>
2020-09-29 13:46:12 -0700

    bump version

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ NAVBAR = dbc.Navbar(
         ),
         dbc.Nav(
             [
-                dbc.NavItem(dbc.NavLink("GNPS LCMS Dashboard - Version 0.6", href="#")),
+                dbc.NavItem(dbc.NavLink("GNPS LCMS Dashboard - Version 0.7", href="#")),
             ],
         navbar=True)
     ],
