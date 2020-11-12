9ced21b9cd9ee2ce69dd0f460faafd82a7e2a343
Mingxun Wang <mwang87@gmail.com>
2020-11-12 11:31:34 -0800

    adding support for google analytics

--- app.py
+++ app.py
@@ -56,6 +56,33 @@ cache = Cache(app.server, config={
 })
 server = app.server
 
+app.index_string = """<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JGX1MKR163"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-JGX1MKR163');
+        </script>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>"""
+
 df = pd.DataFrame()
 df["rt"] = [1]
 df["mz"] = [1]
