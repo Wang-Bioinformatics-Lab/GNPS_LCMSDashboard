a0df7e960b062b58a5b501d8190f9d84b8e36a14
Mingxun Wang <mwang87@gmail.com>
2020-12-11 10:32:12 -0800

    adding test cython

---
+++ notebooks/cython/read_mzml_cython.pyx
@@ -0,0 +1,15 @@
+import pymzml
+
+def read_pymzml_cython(filename):
+    run = pymzml.run.Reader(filename)
+
+    for spec in run:
+        if spec.ms_level == 1:
+            rt = spec.scan_time_in_minutes()
+            peaks = spec.reduce(mz_range=(0, 2000))
+
+            # Sorting by intensity
+            peaks = peaks[peaks[:,1].argsort()]
+            peaks = peaks[-100:]
+
+            mz, intensity = zip(*peaks)
