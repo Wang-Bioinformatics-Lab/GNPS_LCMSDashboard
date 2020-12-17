fb3350d5f78f161508b8e676c707b5f4662a5f03
Mingxun Wang <mwang87@gmail.com>
2020-12-17 14:53:29 -0800

    bug fix

--- test/test_usi_resolution.py
+++ test/test_usi_resolution.py
@@ -3,12 +3,16 @@ sys.path.insert(0, "..")
 import xic
 import pandas as pd
 import download
+import os
 
 def test_resolve():
     df = pd.read_csv("usi_list.tsv", sep='\t')
     for record in df.to_dict(orient="records"):
         print(record["usi"])
         remote_link, local_filename = download._resolve_usi(record["usi"])
+
+        assert(os.path.exists(local_filename))
+        
 
 def test_resolve_filename():
     df = pd.read_csv("usi_list.tsv", sep='\t')
