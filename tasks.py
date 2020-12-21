ccbcb97929365b89c55d805c139cd517a890de4f
Mingxun Wang <mwang87@gmail.com>
2020-12-20 23:03:03 -0800

    adding caching

--- tasks.py
+++ tasks.py
@@ -4,6 +4,8 @@ import os
 import uuid
 import feature_finding
 import xic
+from joblib import Memory
+memory = Memory("/app/temp/xic-cache", verbose=0)
 
 # Setting up celery
 celery_instance = Celery('lcms_tasks', backend='redis://redis', broker='redis://redis')
@@ -22,9 +24,12 @@ def _download_convert_file(usi, temp_folder="temp"):
 #################################
 # Compute Data
 #################################
-@celery_instance.task(time_limit=30)
+@celery_instance.task(time_limit=60)
 def task_xic(local_filename, all_xic_values, xic_tolerance, xic_ppm_tolerance, xic_tolerance_unit, rt_min, rt_max, polarity_filter, get_ms2=False):
-    xic_df, ms2_data = xic.xic_file(local_filename, all_xic_values, xic_tolerance, xic_ppm_tolerance, xic_tolerance_unit, rt_min, rt_max, polarity_filter, get_ms2=get_ms2)
+    # Caching
+    xic_file = memory.cache(xic.xic_file)
+
+    xic_df, ms2_data = xic_file(local_filename, all_xic_values, xic_tolerance, xic_ppm_tolerance, xic_tolerance_unit, rt_min, rt_max, polarity_filter, get_ms2=get_ms2)
     return xic_df.to_dict(orient="records"), ms2_data
 
 @celery_instance.task(time_limit=60)
