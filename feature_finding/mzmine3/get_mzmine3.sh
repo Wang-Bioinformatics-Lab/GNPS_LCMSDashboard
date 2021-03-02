e3bcee6090e166a62a10ccd7f5a6dae2657761f0
Robin Schmid <rschmid1789@gmail.com>
2021-03-02 21:57:56 +0100

    Update download of latest MZmine 3

--- feature_finding/mzmine3/get_mzmine3.sh
+++ feature_finding/mzmine3/get_mzmine3.sh
@@ -1,11 +1,12 @@
 # download mzmine-ubuntu artifact for latest build
-wget --no-check-certificate https://nightly.link/robinschmid/mzmine3/actions/artifacts/44340837.zip
+# wget --no-check-certificate https://nightly.link/robinschmid/mzmine3/actions/artifacts/44356954.zip
 # download latest release
-# wget https://github.com/mzmine/mzmine2/releases/download/v2.53/MZmine-2.53-Linux.zip
+wget https://github.com/robinschmid/mzmine3/releases/download/v3-beta1/MZmine_package_ubuntu-latest.zip
 
 # clear downloads
 rm -rf ./tmp_download
-unzip -d ./tmp_download/ 44340837.zip
+# unzip -d ./tmp_download/ 44356954.zip
+unzip -d ./tmp_download/ MZmine_package_ubuntu-latest.zip
 
 # install xdg dependency
 # somehow on WSL xdg needed this directory - otherwise error
