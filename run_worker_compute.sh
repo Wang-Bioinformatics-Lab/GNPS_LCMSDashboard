305f1d5b761b4417f77f607518a40371dc478bf9
Mingxun Wang <mwang87@gmail.com>
2021-03-24 08:41:27 -0700

    setting parallelism for bigger server

--- run_worker_compute.sh
+++ run_worker_compute.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-celery -A tasks worker -l info -c 6 -Q compute --max-tasks-per-child 10 --loglevel INFO --beat --max-memory-per-child 3000000
+celery -A tasks worker -l info -c 16 -Q compute --max-tasks-per-child 10 --loglevel INFO --beat --max-memory-per-child 3000000
 
