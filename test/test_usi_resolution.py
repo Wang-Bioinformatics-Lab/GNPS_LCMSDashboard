7c626ab23cc515bfe51b2ac5af8ea09574716527
Mingxun Wang <mwang87@gmail.com>
2021-04-04 10:44:26 -0700

    adding fix

--- test/test_usi_resolution.py
+++ test/test_usi_resolution.py
@@ -4,7 +4,9 @@ import xic
 import pandas as pd
 import download
 import os
+import lcms_map
 
+# Testing remote link calculation
 def test_resolve_remote_url():
     df = pd.read_csv("usi_list.tsv", sep='\t')
     for record in df.to_dict(orient="records"):
@@ -12,7 +14,7 @@ def test_resolve_remote_url():
         remote_link = download._resolve_usi_remotelink(record["usi"])
         assert(len(remote_link) > 0)
 
-# Making sure we can convert it
+# Testing conversion
 def test_resolve_download_convert():
     df = pd.read_csv("usi_list.tsv", sep='\t')
     for record in df.to_dict(orient="records"):
@@ -21,9 +23,27 @@ def test_resolve_download_convert():
 
         assert(os.path.exists(local_filename))
 
-# Testing Filenames
+# Testing we can make a feather file
+def test_feather_download_convert():
+    df = pd.read_csv("usi_list.tsv", sep='\t')
+    for record in df.to_dict(orient="records"):
+        print(record["usi"])
+        remote_link, local_filename = download._resolve_usi(record["usi"])
+        output_ms1_filename, output_msn_filename = lcms_map._get_feather_filenames(local_filename)
+        lcms_map._save_lcms_data_feather(local_filename)
+
+        # Making sure the filename exists
+        assert(os.path.exists(output_ms1_filename))
+
+        # Making sure it includes polarity
+        ms1_results = pd.read_feather(output_ms1_filename)
+        assert("polarity" in ms1_results)
+        
+
+# Testing to local filenames
 def test_resolve_filename():
     df = pd.read_csv("usi_list.tsv", sep='\t')
     for record in df.to_dict(orient="records"):
         converted_filename = download._usi_to_local_filename(record["usi"])
         print(record["usi"], converted_filename)
+
