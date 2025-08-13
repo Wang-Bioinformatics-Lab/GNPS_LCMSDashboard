cb888d563f782860b6b398ce6a29df09390c6700
Mingxun Wang <mwang87@gmail.com>
2025-08-13 11:57:51 -0700

    updating

--- download.py
+++ download.py
@@ -384,6 +384,7 @@ def _resolve_usi(usi, temp_folder="temp", cleanup=True):
         try:
             if resource_name == "MASSIVEDATASET":
                 local_dataset_path = "/data/datasets/server/{}/{}".format(usi_splits[1], usi_splits[2])
+                print("DEBUG: TRYING LOCAL", local_dataset_path, file=sys.stderr, flush=True)
                 if os.path.exists(local_dataset_path):
                     os.system("cp {} {}".format(local_dataset_path, temp_download_filename))
                 else:
